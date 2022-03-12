34d73d8bb05309ab703111f0832afb208d0ff617
Bonfim Justino do Nascimento <bonfim1999@gmail.com>
2022-03-12 18:28:13 -0300

    Integraçao com Stripe, para renderizaçao de preço dinamico com StaticSiteGeneration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-// import styled from '../styles/global.scss';
+import {  GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss'
 
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
 
   return (
     <>
@@ -16,15 +25,37 @@ export default function Home() {
           <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
-            Ger access to all the publications <br />
-            <span>for $9.90 month</span>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding" />
+        <img src="/images/avatar.svg" alt="Girl Coding" />
 
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve(process.env.PRICE_SUBSCRIPTION_KEY);  
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat(
+      'pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      },
+    ).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24h
+  };
 }
